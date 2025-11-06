7d778b8f5d89d24ca016169960e3ef08b35ede46
Plranha <89987582+Plranha@users.noreply.github.com>
2025-11-06 09:05:11 -0500

    Add files via upload

---
+++ task_2/task_2.py
@@ -0,0 +1,21 @@
+import re
+
+def generator_numbers(text: str):
+    pattern = r'\s\d+\.?\d*\s'
+
+    for match in re.finditer(pattern, text):
+        number_str = match.group().strip() 
+        number = float(number_str)
+        yield number
+        
+
+def sum_profit(text: str,func: callable):
+    total = 0
+    for number in func(text):
+        total += number
+    return total
+    
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+result = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {result}")
